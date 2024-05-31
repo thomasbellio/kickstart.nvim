aa2414014f2d91a127467bcdc0882d1f93cb17ea
Thomas A Bell <git@thomasbell.io>
2024-05-31 22:05:42 +0000

    configured dap

--- init.lua
+++ init.lua
@@ -154,6 +154,51 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- Keymaps for nvim-dap
+vim.fn.sign_define('DapBreakpoint', { text = '🛑', texthl = '', linehl = '', numhl = '' })
+
+vim.keymap.set('n', '<F5>', function()
+  require('dap').continue()
+end)
+vim.keymap.set('n', '<F10>', function()
+  require('dap').step_over()
+end)
+vim.keymap.set('n', '<F11>', function()
+  require('dap').step_into()
+end)
+vim.keymap.set('n', '<F12>', function()
+  require('dap').step_out()
+end)
+vim.keymap.set('n', '<Leader>b', function()
+  require('dap').toggle_breakpoint()
+end)
+vim.keymap.set('n', '<Leader>B', function()
+  require('dap').set_breakpoint()
+end)
+vim.keymap.set('n', '<Leader>lp', function()
+  require('dap').set_breakpoint(nil, nil, vim.fn.input 'Log point message: ')
+end)
+vim.keymap.set('n', '<Leader>dr', function()
+  require('dap').repl.open()
+end)
+vim.keymap.set('n', '<Leader>dl', function()
+  require('dap').run_last()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dh', function()
+  require('dap.ui.widgets').hover()
+end)
+vim.keymap.set({ 'n', 'v' }, '<Leader>dp', function()
+  require('dap.ui.widgets').preview()
+end)
+vim.keymap.set('n', '<Leader>df', function()
+  local widgets = require 'dap.ui.widgets'
+  widgets.centered_float(widgets.frames)
+end)
+vim.keymap.set('n', '<Leader>ds', function()
+  local widgets = require 'dap.ui.widgets'
+  widgets.centered_float(widgets.scopes)
+end)
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.
 --  vim.keymap.set takes the following arguments
@@ -259,7 +304,64 @@ require('lazy').setup({
   --
   --  This is equivalent to:
   --    require('Comment').setup({})
-  { 'mfussenegger/nvim-dap', opts = {} },
+  {
+    'leoluz/nvim-dap-go',
+    opts = {},
+    -- config = function()
+    --   local dapGo = require 'dap-go'
+    --   dapGo.setup {
+    --     dap_configurations = {
+    --       type = 'go',
+    --       name = 'Atach remote',
+    --       mode = 'remote',
+    --       request = 'atach',
+    --     },
+    --     -- delve configurations
+    --     delve = {
+    --       -- the path to the executable dlv which will be used for debugging.
+    --       -- by default, this is the "dlv" executable on your PATH.
+    --       path = 'dlv',
+    --       -- time to wait for delve to initialize the debug session.
+    --       -- default to 20 seconds
+    --       initialize_timeout_sec = 20,
+    --       -- a string that defines the port to start delve debugger.
+    --       -- default to string "${port}" which instructs nvim-dap
+    --       -- to start the process in a random available port
+    --       port = '${port}',
+    --       -- additional args to pass to dlv
+    --       args = {},
+    --       -- the build flags that are passed to delve.
+    --       -- defaults to empty string, but can be used to provide flags
+    --       -- such as "-tags=unit" to make sure the test suite is
+    --       -- compiled during debugging, for example.
+    --       -- passing build flags using args is ineffective, as those are
+    --       -- ignored by delve in dap mode.
+    --       build_flags = '',
+    --       -- whether the dlv process to be created detached or not. there is
+    --       -- an issue on Windows where this needs to be set to false
+    --       -- otherwise the dlv server creation will fail.
+    --       detached = true,
+    --       -- the current working directory to run dlv from, if other than
+    --       -- the current working directory.
+    --       cwd = nil,
+    --     },
+    --   }
+    -- end,
+  },
+  {
+    'mfussenegger/nvim-dap',
+    opts = {},
+    config = function()
+      local dap = require 'dap'
+      dap.adapters.go = {
+        type = 'server',
+        executable = {
+          command = 'dlv',
+          args = { 'debug', '${file}' },
+        },
+      }
+    end,
+  },
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
 
